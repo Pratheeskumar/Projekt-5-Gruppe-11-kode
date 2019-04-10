a6687ff9693266b56093c7969b6d6c7b6a7a05cf
Prathees <prat0025@edu.eal.dk>
2019-04-10 11:39:21 +0200

    Restart
    Started from the bottom and we're still here

--- java.js
+++ java.js
@@ -1,1 +1,33 @@
+let sliderList = [];
+sliderList[0] = "";
+sliderList[1] = "cat-pics/cat2.jpg";
+sliderList[2] = "cat-pics/cat3.jpg";
+sliderList[3] = "cat-pics/cat4.jpg";
 
+let counter = 0;
+document.getElementById("sliderInfo").innerText = (counter +1) + " of " + sliderList.length;
+
+// Event Listener ? Handler
+document.getElementById("btnNext").addEventListener("click", function() {
+  counter++;
+
+  // Reset counter
+  if (counter >= sliderList.length) {
+    counter = 0;
+  }
+
+  document.getElementById("sliderImage").src = sliderList[counter];
+  document.getElementById("sliderInfo").innerText = (counter +1) + " of " + sliderList.length;
+});
+
+document.getElementById("btnPrev").addEventListener("click", function() {
+  counter--;
+
+  // Reset counter
+  if (counter <0) {
+    counter = sliderList.length -1;
+  }
+
+  document.getElementById("sliderImage").src = sliderList[counter];
+  document.getElementById("sliderInfo").innerText = (counter +1) + " of " + sliderList.length;
+});
