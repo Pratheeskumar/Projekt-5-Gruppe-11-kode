6c54dac12acbddb07d96a0d28ac55490fe921d57
Prathees <prat0025@edu.eal.dk>
2019-04-11 12:12:17 +0200

    JEg har stylet og rettet lidt mere til

--- java.js
+++ java.js
@@ -1,7 +1,7 @@
-var slideIndex = 1;
+let slideIndex = 1;
 showSlides(slideIndex);
 
-// Next/previous controls
+// Styring af next/previous
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
@@ -12,9 +12,9 @@ function currentSlide(n) {
 }
 
 function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1} 
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
