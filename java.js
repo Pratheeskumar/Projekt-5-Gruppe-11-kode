07aef10929eb754d9229b5d620c4e92f7b728dce
Thusitharan <thusitharan93@gmail.com>
2019-04-13 21:54:16 +0700

    nyt

--- java.js
+++ java.js
@@ -26,3 +26,25 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block"; 
   dots[slideIndex-1].className += " active";
 }
+
+
+// Challenge side tab-skift 
+var tabButtons=document.querySelectorAll(".valgboks .klikbokser button") 
+var tabPanels=document.querySelectorAll(".valgboks .panel") 
+
+function showPanel (panelIndex,colorCode) {
+  tabButtons.forEach (function(node){
+    node.style.backgroundColor="";
+    node.style.color="";
+  });
+
+  tabButtons[panelIndex].style.backgroundColor=colorCode;
+  tabButtons[panelIndex].style.color= "white"; 
+
+  tabPanels.forEach(function(node){
+    node.style.display="none";
+  });
+
+  tabPanels[panelIndex].style.display= "block"; 
+  tabPanels[panelIndex].style.backgroundColor= colorCode; 
+}
