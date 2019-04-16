af44ffed5ae024d3c9eb958eff941cfdc73cbc84
Prathees <prat0025@edu.eal.dk>
2019-04-16 10:51:58 +0200

    something new

--- totalscoresldr.js
+++ totalscoresldr.js
@@ -1,55 +1,14 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+let slideIndex = 0;
+carousel();
 
-// Styring af next/previous
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
+function carousel() {
   let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1} 
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none"; 
+  let x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none"; 
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block"; 
-  dots[slideIndex-1].className += " active";
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1} 
+  x[slideIndex-1].style.display = "block"; 
+  setTimeout(carousel, 2000); // Change image every 2 seconds
 }
-
-
-// Challenge side tab-skift 
-var tabButtons=document.querySelectorAll(".valgboks .klikbokser button") 
-var tabPanels=document.querySelectorAll(".valgboks .panel") 
-
-function showPanel (panelIndex,colorCode) {
-  tabButtons.forEach (function(node){
-    node.style.backgroundColor="";
-    node.style.color="";
-  });
-
-  tabButtons[panelIndex].style.backgroundColor=colorCode;
-  tabButtons[panelIndex].style.color= "white"; 
-
-  tabPanels.forEach(function(node){
-    node.style.display="none";
-  });
-
-  tabPanels[panelIndex].style.display= "block"; 
-  tabPanels[panelIndex].style.backgroundColor= colorCode; 
-}
-
-
-
-//canvas til pie chart
-
