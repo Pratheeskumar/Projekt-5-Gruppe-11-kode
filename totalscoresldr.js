b04bb10e4046deb8b498f8606ccfacb8dd7c5748
Prathees <prat0025@edu.eal.dk>
2019-04-21 11:01:11 +0200

    Turtle Turtle Turtle Turtle Turtle Turtle Turtle Turtle Turtle Turtle Turtle Turtle Turtle Turtle Turtle Turtle Turtle Turtle Turtle Turtle Turtle Turtle Turtle Turtle Turtle Turtle Turtle Turtle Turtle Turtle Turtle Turtle Turtle Turtle Turtle Turtle Turtle Turtle Turtle Turtle Turtle Turtle Turtle Turtle Turtle Turtle Turtle Turtle Turtle Turtle Turtle Turtle Turtle Turtle Turtle Turtle Turtle Turtle Turtle Turtle Turtle Turtle Turtle Turtle Turtle Turtle

--- totalscoresldr.js
+++ totalscoresldr.js
@@ -10,7 +10,6 @@ function plusSlides(n) {
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
-
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
@@ -20,9 +19,7 @@ function showSlides(n) {
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none"; 
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
+
   slides[slideIndex-1].style.display = "block"; 
   dots[slideIndex-1].className += " active";
 }
